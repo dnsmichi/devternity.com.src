d0ed8c57d1b0c6aba822ef46ac4a634ef8063d85
sizovs <eduards@sizovs.net>
2022-12-14 13:55:19 +0000

    Deploying to gh-pages from @ devternity/devternity.com.src@afa6261406280447ed5d835e6fd084d07146f1b7 🚀

--- js/event.js
+++ js/event.js
@@ -10,7 +10,7 @@
     "city": "Online",
     "venue_name": "GMT (London Time)",
     "contactEmail": "hello@devternity.com",
-    "status": "on_sale",
+    "status": "sold_out",
     "country": "Latvia",
     "classes": "extended_event",
     "time": "8:00",
